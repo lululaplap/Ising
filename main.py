5bc65cc6904e535937b7060c9e3fedb630059fab
Lewis Lappin <s1516989@cplab004.ph.ed.ac.uk>
2019-02-06 18:38:21 +0000

    working glauber dynamics

--- main.py
+++ main.py
@@ -34,9 +34,10 @@ class Ising(object):
 
     def sweep(self):
         dE = 0
-        for i in range(0,self.N):
+        for i in range(0,self.N**2):
             dE += self.flip()
         return dE
+
     def totalEnergy(self):
         E= 0
         for i in range(self.N):
@@ -50,29 +51,36 @@ class Ising(object):
             self.sweep()
 
     def simulate(self,n,animate=False):
-        self.equ(1000)
+        self.equ(100)
+        print(self.T)
         Es = np.zeros(n,dtype=float)
         Es[0] = self.totalEnergy()
         Ms = np.zeros(n,dtype=float)
         Ms[0] = self.totalM()
+        Et = 0
+        Mt = 0
+        E2 = 0
+        M2 = 0
         for i in range(1,n):
-            Es[i] = self.sweep()+Es[i-1]
+            self.sweep()
+            Es[i] = self.totalEnergy()
             Ms[i] = self.totalM()
+            Et += Es[i]
+            Mt += Ms[i]
+            E2 += Es[i]**2
+            M2 += Ms[i]**2
             if i%50 == 0 & animate==True:
                 plt.imshow(self.spins)
                 plt.pause(0.005)
 
-        if animate:
+        if animate==True:
             plt.show()
         C = np.var(Es)/(self.N**2*self.T**2)
-        X = np.var(Ms)/(self.N*self.T)
+        X = np.var(Ms)/(self.N**2*self.T)
         E = np.mean(Es)
         M=np.mean(Ms)
-        return [C,X,E,M]
-        #plt.plot(Es)
-        #plt.show()
-        #plt.plot(Ms)
-        #plt.show()
+        return [C,X,E,M,Et,Mt,E2,M2]
+
 
 
     def totalM(self):
@@ -93,20 +101,65 @@ class Ising(object):
         Xs = np.zeros(n)
         Es = np.zeros(n)
         Ms = np.zeros(n)
+        Et = 0
+        Mt = 0
         for i in range(0,n):
             I = Ising(N,p,temps[i])
-            [C,X,E,M] = I.simulate(10000)
+            [C,X,E,M,Et,Mt,E2,M2] = I.simulate(10000)
 
-            Cs[i] = C
-            Xs[i] = X
             Es[i] = E
             Ms[i] = M
-        plt.plot(temps,Cs)
-        plt.show()
-def main():
-    I = Ising(100,0.5,1)
-    Ising.experiment(30,0.5,30)
-    #for i in range(0,1):
-    #    I.simulate(10000)
+            Cs[i] = C#E2-Et*2
+            Xs[i] = X#M2-Mt*2
 
+        np.savetxt('heatcapacity.csv',Cs,delimiter=',')
+        np.savetxt('energy.csv',Es,delimiter=',')
+        np.savetxt('temperatures.csv',temps,delimiter=',')
+        np.savetxt('mag.csv',Ms,delimiter=',')
+        np.savetxt('magsep.csv',Xs,delimiter=',')
+
+    @staticmethod
+    def plots():
+        C = np.genfromtxt('heatcapacity.csv', delimiter=",")
+        E = np.genfromtxt('energy.csv', delimiter=",")
+        T = np.genfromtxt('temperatures.csv', delimiter=",")
+        M = np.genfromtxt('mag.csv', delimiter=",")
+        X = np.genfromtxt('magsep.csv', delimiter=",")
+        f = plt.figure(figsize=(18, 10))
+
+        sp =  f.add_subplot(2, 2, 1 );
+        plt.scatter(T,E)
+        plt.plot(T,E)
+        plt.xlabel("Temperature (T)", fontsize=20);
+        plt.ylabel("Energy ", fontsize=20);         plt.axis('tight');
+
+        sp =  f.add_subplot(2, 2, 2 );
+        plt.scatter(T,C)
+        plt.plot(T,C)
+        plt.xlabel("Temperature (T)", fontsize=20);
+        plt.ylabel("Specific Heat ", fontsize=20);         plt.axis('tight');
+
+        sp =  f.add_subplot(2, 2, 3 );
+        plt.scatter(T,abs(M))
+        plt.plot(T,abs(M))
+        plt.xlabel("Temperature (T)", fontsize=20);
+        plt.ylabel("Magnetization ", fontsize=20);         plt.axis('tight');
+
+        sp =  f.add_subplot(2, 2, 4 );
+        plt.scatter(T,abs(X))
+        plt.plot(T,abs(X))
+        plt.xlabel("Temperature (T)", fontsize=20);
+        plt.ylabel("Susceptibility", fontsize=20);         plt.axis('tight');
+
+        plt.show()
+
+
+
+def main():
+    I = Ising(10,0.5,1)
+    Ising.experiment(20,0.99,20)
+    Ising.plots()
+    #for i in range(0,1):
+        #I.simulate(10000,True)
+#
 main()
